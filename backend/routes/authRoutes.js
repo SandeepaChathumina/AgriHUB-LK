a32a531b6c2861964180ceb184580dd40afbde70
theshanGeeth123 <theshangeethanjana@gmail.com>
2026-02-24 21:59:21 +0530

    User access for Routes have protected

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,8 @@ import express from 'express';
 // Make sure to add getAllUsers to your import list!
 import { register, login, getAllUsers,testEmail,verifyEmail,requestVerificationOTP,forgotPassword, resetPassword } from '../controllers/authController.js';
 
+import { protect, authorizeRoles } from '../middleware/authMiddleware.js';
+
 const router = express.Router();
 
 // POST /api/auth/register
@@ -10,8 +12,7 @@ router.post('/register', register);
 // POST /api/auth/login
 router.post('/login', login);
 
-// GET /api/auth/users (Test endpoint to view all registered users)
-router.get('/users', getAllUsers);
+
 
 // POST /api/auth/test-email (Temporary)
 router.post('/test-email', testEmail);
@@ -23,5 +24,7 @@ router.post('/request-otp', requestVerificationOTP);
 router.post('/forgot-password', forgotPassword);
 
 router.post('/reset-password', resetPassword);
+
+router.get('/users', protect, authorizeRoles('Admin'), getAllUsers);
 
 export default router;
